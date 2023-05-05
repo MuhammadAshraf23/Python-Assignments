377ed999cb88e0bd0737e956f5b4792c206c0b23
Muhammad Ashraf <121554755+MuhammadAshraf23@users.noreply.github.com>
2023-05-05 12:04:14 +0500

    dk

--- Assignments/index3.py
+++
@@ -1,6 +0,0 @@
-a=input('Enter the original Selling Price:')
-originalsellingprice=float(a)
-b=input('Enter the discounted Selling Percentage:')
-discountsellingpercentage=float(b)
-discountsellingprice=originalsellingprice-(originalsellingprice*discountsellingpercentage/100)
-print('The Discounted Selling Price is: %.2f' %(discountsellingprice))
