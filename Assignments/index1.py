6d1ddfa2da758ab4fc7de6371afdc162bfebfe4c
Muhammad Ashraf <121554755+MuhammadAshraf23@users.noreply.github.com>
2023-05-05 16:52:32 +0500

    update

--- Assignments/index1.py
+++ Assignments/index1.py
@@ -1,22 +1,22 @@
-# Q1 Write a program that prints a percentage marks of high school graduates by taking input of sum of their obtained marks and maximum marks.
+               # Q1 Write a program that prints a percentage marks of high school graduates by taking input of sum of their obtained marks and maximum marks.
 a=input('Enter your Obtained Marks: ')
 obtained=float(a)
 b=input('Enter Your Maximum/Total Marks:')
 totalmark=float(b)
 percentage=obtained/totalmark*100
 print("Your Percentage is: %.2f" % (percentage))
+                    ############################QUESTION END#########################################
 
-
-#Q2 Write a program that takes input of radius of circle and prints its circumference and area.
+                              #Q2 Write a program that takes input of radius of circle and prints its circumference and area.
 a=input('Enter the radius of Circle:')
 r=float(a)
 circumference=2*3.14*r
 area=3.14*r**2
 print('The Circumference of Circle is: %.2f' % (circumference))
 print('The Area of the Circle is: %.2f' % (area)) 
+                  ############################QUESTION END#########################################
 
-
-#Q3 Discounted selling price is to be calculated if original selling price and discount percentage is given. Write a program to 
+                #Q3 Discounted selling price is to be calculated if original selling price and discount percentage is given. Write a program to 
 #calculate and print discounted selling priceof the user given two inputs  1)Original selling price  2)Discounted selling price
 a=input('Enter the original Selling Price:')
 originalsellingprice=float(a)
@@ -24,8 +24,9 @@ b=input('Enter the discounted Selling Percentage:')
 discountsellingpercentage=float(b)
 discountsellingprice=originalsellingprice-(originalsellingprice*discountsellingpercentage/100)
 print('The Discounted Selling Price is: %.2f' %(discountsellingprice))
-
-#Q4 Write a program to calculate and print the original selling price if the discounted selling price and discount percentage is entered by the user.
+                     ############################QUESTION END#########################################
+  
+                        #Q4 Write a program to calculate and print the original selling price if the discounted selling price and discount percentage is entered by the user.
 a=input('Enter the discounted Selling Price:')
 discountedsellingprice=float(a)
 b=input('Enter the discounted Percentage:')
